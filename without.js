f70ef166ebe5de1a7dcd4ecb50e599536008fed9
lucas <lucas.johannson1@ucalgary.ca>
2021-05-25 21:48:46 +0000

    without

---
+++ without.js
@@ -0,0 +1,35 @@
+// FUNCTION IMPLEMENTATION
+const assertArraysEqual = function(a1, a2) {
+  if (eqArrays) {
+    console.log(`✅✅✅Assertion Passed: ${a1} === ${a2}`);
+  } else {
+    console.log(`🤬🤬🤬 Assertion Failed: ${a1} !== ${a2}`);
+  }
+};
+
+
+function eqArrays(arr1, arr2){
+  if(arr1.length !== arr2.length) return false;
+  for(let i = 0; i < arr1.length; i ++){
+    if(arr1[i] !== arr2[i]){
+      return false;
+    }
+  }
+  return true;
+}
+
+function without(source, toRemove){
+  let removed = [];
+  for(let i = 0; i < source.length; i ++){
+    if(toRemove.includes(source[i])){
+      continue;
+    }else{
+      removed.push(source[i]);
+    }
+  }
+  return removed;
+}
+
+assertArraysEqual(eqArrays(without([1, 2, 3], [1]), [2, 3]), true); // => should PASS
+assertArraysEqual(eqArrays(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]), true); // => should PASS
+assertArraysEqual(eqArrays(without(["1", "2", "3"], [1, 2, "3"]), ["2"]), false); // => should PASS
