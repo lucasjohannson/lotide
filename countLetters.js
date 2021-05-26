604502267c1211c383a3426d20cfcbdd8d0a2ded
lucas <lucas.johannson1@ucalgary.ca>
2021-05-26 20:12:33 +0000

    letter count

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤬🤬🤬 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+// inp: LHL out: L -> 2 H -> 1
+countLetters = function(sentence){
+  letterCount = {}
+  for(let letter of sentence){
+    if(letter === ' '){
+      continue;
+    }
+    if(letterCount[letter]){
+      letterCount[letter] += 1;
+    }
+    else{
+      letterCount[letter] = 1;
+    }
+  }
+  return letterCount;
+
+}
+
+console.log(countLetters("lighthouse in the house"));
