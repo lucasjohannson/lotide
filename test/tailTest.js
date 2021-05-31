5c4e6c9cf3fea62201dcecd0da35c111ab7ac9e5
lucas <lucas.johannson1@ucalgary.ca>
2021-05-31 19:05:29 +0000

    refactor tail for modularity

---
+++ test/tailTest.js
@@ -0,0 +1,5 @@
+const assertEqual = require('../assertEqual');
+const tail = require('../tail');
+// TEST CODE
+assertEqual(tail([5,6,7])[0], 6);
+assertEqual(tail(["Hello", "Lighthouse", "Labs"]).length, 2);
